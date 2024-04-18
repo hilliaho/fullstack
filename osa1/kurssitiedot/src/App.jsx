a94dc78123bc13ed67a5c83e2897420ee3f10be6
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2024-04-18 10:52:14 +0300

    Tehtävät 1.1 & 1.2

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part part={props.parts[0].part} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].part} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].part} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total e1={parts[0].exercises} e2={parts[1].exercises} e3={parts[2].exercises} />
+    </div>
+  )
+}
+
+export default App
