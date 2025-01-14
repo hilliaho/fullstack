b00e819ff3f41dc007c4748c63391e226eb0241a
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-14 14:50:22 +0200

    Tehtävä 4.2

---
+++ osa4/blogilista/models/blog.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 4
+  },
+  likes: {
+    type: Number,
+    required: true
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
