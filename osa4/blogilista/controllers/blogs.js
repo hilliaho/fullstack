098b24a55b3b600910517a144fa7d76879fbc2cc
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-15 14:18:26 +0200

    Tehtävä 4.8

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then(blogs => {
-        response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
     const blog = new Blog({
         title: body.title,
@@ -16,10 +15,8 @@ blogsRouter.post('/', (request, response, next) => {
         likes: body.likes
     })
 
-    blog.save()
-    .then(savedBlog => {
-        response.json(savedBlog)
-    })
+    savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
     .catch(error => next(error))
 })
 
