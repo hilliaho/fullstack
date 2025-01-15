824eefa996ece4575e5d509d03f943e797ef2651
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-15 14:48:41 +0200

    Tehtävä 4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,53 +1,32 @@
 const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
-const Blog = require('../models/blog')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
-
-const initialBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        __v: 0
-      },
-      {
-        _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5,
-        __v: 0
-      },
-      {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
-        __v: 0
-      }
-]
+const {initialBlogs, blogsInDb} = require('./test_helper')
+const Blog = require('../models/blog')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
+    const blogObjects = initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+    
 })
 
-test.only('right amount of blogs are returned as json', async () => {
+test('right amount of blogs are returned as json', async () => {
   const response = await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test.only('identifier field name is id', async () => {
+    const allBlogs = await blogsInDb()
+    const keys = Object.keys(allBlogs[0])
+    assert(!keys.includes("_id") && keys.includes("id"))
 })
 
 after(async () => {
