c519b850b15db0e97e63c3c0c4d0df45daee4e20
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-16 14:10:56 +0200

    Tehtävä 4.15

--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -51,11 +51,22 @@ const initialBlogs = [
       } 
 ]
 
+const initialUsers = [
+  {
+
+  }
+]
+
 const blogsInDb = async () => {
     const blogs = await Blog.find({})
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-    initialBlogs, blogsInDb
+    initialBlogs, blogsInDb, initialUsers, usersInDb
 }
