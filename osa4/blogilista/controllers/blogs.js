6da3c505dc88cf2270fee1e4c564d13870fc7805
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-20 17:05:46 +0200

    Tehtävä 5.8

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -53,16 +53,18 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
     const body = request.body
+    const user = request.user
 
     const blog = {
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
-        id: body.id
+        id: body.id,
+        user: user.id
     }
-    await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
-    response.json(200).end()
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    response.status(200).json(updatedBlog)
 })
 
 module.exports = blogsRouter
