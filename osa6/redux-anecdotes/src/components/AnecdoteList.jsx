c4e59ecd0eff7f8ca419613dd0f23ffaa6249d49
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2025-01-22 19:16:27 +0200

    Tehtävät 6.12-6.13

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,15 +1,20 @@
 import { useDispatch, useSelector } from "react-redux"
 import { vote } from '../reducers/anecdoteReducer'
+import { setNotification } from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
   const anecdotes = useSelector(state => {
     return state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(state.filter.toLowerCase()))
   })
-
-
+  
   const handleVote = (id) => {
     dispatch(vote(id))
+    const anecdote = anecdotes.find(a => a.id === id)
+    dispatch(setNotification(`you voted ${anecdote.content}`))
+    setTimeout(() => {
+      dispatch(setNotification(''))
+    }, 5000)
   }
 
   return (
--- osa6/redux-anecdotes/store.js
+++ osa6/redux-anecdotes/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { composeWithDevTools } from 'redux-devtools-extension'
 import anecdoteReducer from "./src/reducers/anecdoteReducer";
 import filterReducer from "./src/reducers/filterReducer";
+import notificationReducer from "./src/reducers/notificationReducer";
 
 const store = configureStore({
     reducer: {
         anecdotes: anecdoteReducer,
-        filter: filterReducer
+        filter: filterReducer,
+        notification: notificationReducer
     }, composeWithDevTools
 })
 
